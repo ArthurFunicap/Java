81104516710a4bd3fb0a72f48ffe0e8b916f4992
ArthurFunicap <arthur.00000849887@unicap.br>
2024-05-28 21:40:47 -0300

    New commit

---
+++ Projetos/POO/Banco/Cliente.java
@@ -0,0 +1,33 @@
+package Banco;
+
+public class Cliente{
+    private String nome;
+    private String endereco;
+    private String profissao;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getProfissao() {
+        return profissao;
+    }
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    void exibirAtributos(){
+        System.out.println("Dados do cliente: ");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Endereço: " + getEndereco());
+        System.out.println("Profissão: " + getProfissao());
+    }
+}
---
+++ Projetos/POO/Banco/Conta.java
@@ -0,0 +1,119 @@
+package Banco;
+
+import java.util.Scanner;
+
+class DepositoInvalidoException extends Exception{
+    public DepositoInvalidoException(){
+        super("Valor para depósito inválido!");
+    }
+
+    public DepositoInvalidoException(String mensagem){
+        super(mensagem);
+    }
+}
+
+class SaqueInvalidoException extends Exception{
+    public SaqueInvalidoException(){
+        super("Saque negado!");
+    }
+
+    public SaqueInvalidoException(String mensagem){
+        super(mensagem);
+        System.out.println("Não é possível sacar os centavos do saldo.");
+        System.out.println("Não é possível realizar um saque de uma quantia maior que o disponível na conta.");
+    }
+}
+
+class ContaInexistenteException extends Exception{
+    public ContaInexistenteException(){
+        super("A conta informada não existe!");
+    }
+
+    public ContaInexistenteException(String mensagem){
+        super(mensagem);
+    }
+}
+
+class SaldoInsuficienteException extends Exception{
+    public SaldoInsuficienteException(){
+        super("Saldo insuficente!");
+    }
+
+    public SaldoInsuficienteException(String mensagem){
+        super(mensagem);
+    }
+}
+
+public class Conta {
+    private String numeroAgencia;
+    private String numeroConta;
+    private double saldo;
+    private Cliente cliente;
+
+    public String getNumeroAgencia() {
+        return numeroAgencia;
+    }
+    public void setNumeroAgencia(String numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    void deposito(Double quantia) throws DepositoInvalidoException{
+        if(quantia < 0){
+            throw new DepositoInvalidoException();
+        }
+
+        this.saldo += quantia;
+        System.out.println("Depósito realizado com sucesso!");
+    }
+
+    void saque(Double quantia) throws SaqueInvalidoException{
+        if(quantia > this.saldo){
+            throw new SaqueInvalidoException();
+        }
+
+        this.saldo -= quantia;
+        System.out.println("Saque realizado com sucesso!");
+    }
+
+    void transferencia(Conta conta) throws ContaInexistenteException, SaldoInsuficienteException{
+        if(conta == null){
+            throw new ContaInexistenteException();
+        }
+
+        Scanner input = new Scanner(System.in);
+        double valorTransferencia;
+
+        System.out.print("Digite um valor: ");
+        valorTransferencia = input.nextDouble();
+        input.close();
+
+        if(valorTransferencia > this.saldo){
+            throw new SaldoInsuficienteException();
+        }
+
+        conta.setSaldo(conta.getSaldo()+valorTransferencia);
+    }
+
+    void exibirSaldo(){
+        System.out.println("Nome: " + this.cliente.getNome());
+        System.out.printf("Saldo: %.2f", this.saldo);
+    }
+}
